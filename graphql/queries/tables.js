51684ca270b090f2f7922bc7229f7e1f5c7ec7fd
Yılmaz Oktav <hello@yilmazoktav.com>
2021-06-17 18:07:48 +0300

    Add graphql query

---
+++ graphql/queries/tables.js
@@ -0,0 +1,36 @@
+import gql from 'graphql-tag';
+
+export const TABLE_NAMES = {
+  USER_GROUPS: 'user-groups',
+};
+
+export const TABLE_USER_GROUPS = gql`
+  query tableUserGroups(
+    $searchText: String
+    $searchColumn: String
+    $orderColumn: String
+    $orderDirection: orderEnum
+    $page: Int
+    $limit: Int
+  ) {
+    tableUserGroups(
+      search: { text: $searchText, column: $searchColumn }
+      orderBy: { column: $orderColumn, direction: $orderDirection }
+      page: $page
+      limit: $limit
+    ) {
+      to
+      from
+      total
+      per_page
+      last_page
+      current_page
+      has_more_pages
+      data {
+        id
+        title
+        content
+      }
+    }
+  }
+`;
