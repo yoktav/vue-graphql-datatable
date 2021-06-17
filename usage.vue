deb11e8a8460b8b06fba07436437f42f836d6f52
Yılmaz Oktav <hello@yilmazoktav.com>
2021-06-17 18:07:54 +0300

    Add usage example

---
+++ usage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Datatable :has-search="true" :columns="columns" :query-name="TABLE_NAMES.USER_GROUPS">
+      <template #cell(content)="data">asd {{ data.data }} </template>
+    </Datatable>
+  </div>
+</template>
+
+<script>
+import { TABLE_NAMES } from './graphql/queries/tables';
+
+export default {
+  data() {
+    return {
+      TABLE_NAMES,
+
+        columns: [
+        {
+          key: 'id',
+          title: 'ID',
+          searchable: true,
+          sortable: true,
+        },
+        {
+          key: 'title',
+          title: 'Başlık',
+          searchable: true,
+          sortable: true,
+        },
+        {
+          key: 'content',
+          title: 'İçerik',
+          searchable: true,
+          sortable: false,
+        },
+      ],
+    };
+  },
+};
+</script>
